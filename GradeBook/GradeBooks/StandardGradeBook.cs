b647179105aaa91d2d83c1e1eb4f1b8b8f50dc15
adamflitney <adam.flitney@gmail.com>
2020-04-02 11:41:12 +0100

    Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
