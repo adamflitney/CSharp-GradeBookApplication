0994681b2134eb558027298318dbace960e0cd2f
adamflitney <adam.flitney@gmail.com>
2020-04-02 12:15:57 +0100

    added GetLetterGrade method to RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,43 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int gradeThreshold = Students.Count / 5; // 20% is threshold for each grade
+            List<double> sortedAverageGrades = new List<double>();
+            Students.ForEach(student => sortedAverageGrades.Add(student.AverageGrade));
+            sortedAverageGrades.Sort();
+            sortedAverageGrades.Reverse();
+
+
+            if(averageGrade >= sortedAverageGrades[gradeThreshold])
+            {
+                return 'A';
+            }
+            else if(averageGrade >= sortedAverageGrades[gradeThreshold*2])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= sortedAverageGrades[gradeThreshold * 3])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= sortedAverageGrades[gradeThreshold * 4])
+            {
+                return 'D';
+            }
+            //else if (averageGrade >= sortedAverageGrades[gradeThreshold * 5])
+            //{
+            //    return 'e';
+            //}
+
+            return 'F';
+        }
     }
 }
